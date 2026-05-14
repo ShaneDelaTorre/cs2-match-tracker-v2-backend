f14b59cdc08ee2f37593f9b364f7ead2ee65d4cd
Shane <jc.delatorre@academy.opswerks.com>
2026-05-15 00:08:00 +0800

    feat(auth): update views for auth implement sso

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,23 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.conf import settings
+from authlib.integrations.django_client import OAuth
+from datetime import datetime
+
+User = get_user_model()
+
+oauth = OAuth()
+oauth.register(
+    name="google",
+    client_id=settings.GOOGLE_CLIENT_ID,
+    client_secret=settings.GOOGLE_CLIENT_SECRET,
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={"scope": "openid email profile"},
+)
 
 class CookieTokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
@@ -58,6 +75,69 @@ class CookieTokenRefreshView(TokenRefreshView):
 
         return res
     
+class GoogleLoginView(APIView):
+    permission_classes = []
+
+    def get(self, request):
+        redirect_uri = "http://localhost:8000/auth/google/callback/"
+        return oauth.google.authorize_redirect(request, redirect_uri)
+
+class GoogleCallbackView(APIView):
+    permission_classes = []
+
+    def get(self, request):
+        token = oauth.google.authorize_access_token(request)
+        claims = token.get("userinfo")
+
+        google_id = claims["sub"]
+        email = claims.get("email", "")
+
+        print(token)
+
+        try:
+            user = User.objects.get(google_id=google_id)
+            is_new = False
+        except User.DoesNotExist:
+            base_username = email.split("@")[0]
+            timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+            username = f"{base_username}_{timestamp}"
+
+            user = User.objects.create(
+                username=username,
+                email=email,
+                google_id=google_id
+            )
+            is_new = True
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        frontend_url = "http://localhost:3000"
+        redirect_url = f"{frontend_url}/profile/setup/" if is_new else f"{frontend_url}/dashboard/"
+        response = redirect(redirect_url)
+
+        response.set_cookie(
+            "access_token",
+            access_token,
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+            path="/"
+        )
+
+        response.set_cookie(
+            "refresh_token",
+            refresh_token,
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+            path="/" 
+        )
+
+        return response
+
+
 
 class LogoutView(APIView):
     def post(self, request):
