e3991f1d94770d8c45f788d49cd4e89df752ad95
Shane <jc.delatorre@academy.opswerks.com>
2026-05-15 10:25:01 +0800

    feat(auth): add google sso endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CookieTokenObtainPairView, CookieTokenRefreshView, LogoutView
+from .views import CookieTokenObtainPairView, CookieTokenRefreshView, GoogleLoginView, GoogleCallbackView, LogoutView
 
 urlpatterns = [
     path("jwt/create/", CookieTokenObtainPairView.as_view()),
     path("jwt/refresh/", CookieTokenRefreshView.as_view()),
     path("logout/", LogoutView.as_view()),
+    path("google/login/", GoogleLoginView.as_view()),
+    path("google/callback/", GoogleCallbackView.as_view()),
 ]
